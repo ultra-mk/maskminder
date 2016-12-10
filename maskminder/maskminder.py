3d047b34302a4d81369ed36752ff83665e13f1fc
michaelkunc <mjkunc@gmail.com>
2016-12-10 15:12:59 -0800

    start of frequncies class

--- maskminder/maskminder.py
+++ maskminder/maskminder.py
@@ -1,4 +1,25 @@
+frequencies = {'E': '41hz'}
+
+# this will hold he collection of instruments and keys
+
+
 class MaskMinder(object):
 
+    def __init__(self, tonic):
+        self.tonic = tonic
+
+    @property
+    def tonic_freq(self):
+        return frequencies[self.tonic]
+
+# this will be the parent class that the various instruments
+# inherit from
+# will map notes to frequencies.
+
+
+class Frequencies(object):
+    human_range = range(20, 20001)
+
     def __init__(self):
-        self.stuff = "This is the init method"
+        self.low = Frequencies.human_range[0]
+        self.high = Frequencies.human_range[-1]
--- tests/test_maskminder.py
+++ tests/test_maskminder.py
@@ -7,7 +7,7 @@ class MMTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(MMTest):
-        MMTest.ins = m.MaskMinder()
+        MMTest.ins = m.MaskMinder('E')
 
-    def test_init(self):
-        self.assertEqual("This is the init method", MMTest.ins.stuff)
+    def test_tonic_freq(self):
+        self.assertEqual('41hz', MMTest.ins.tonic_freq)
