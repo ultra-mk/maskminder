af27f837ec58a6e89a28cacabdec00a96b87b04e
michaelkunc <mjkunc@gmail.com>
2016-12-10 18:59:05 -0800

    test quite for Note class

--- tests/test_note.py
+++ tests/test_note.py
@@ -9,3 +9,14 @@ class NTest(unittest.TestCase):
         C2 = n.Note('C', 2)
         self.assertEqual(65.4, C2.frequency)
 
+    def test_A4(self):
+        A4 = n.Note('A', 4)
+        self.assertEqual(440.0, A4.frequency)
+
+    def test_C_sharp_6(self):
+        Csharp = n.Note('C#', 6)
+        self.assertEqual(1108.48, Csharp.frequency)
+
+    def test_d_flat_6(self):
+        Db = n.Note('Db', 6)
+        self.assertEqual(1108.48, Db.frequency)
