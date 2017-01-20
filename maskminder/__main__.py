2161fcfc83a49657cf7a06d9dcd55c308a7bfce3
michaelkunc <mjkunc@gmail.com>
2017-01-19 16:22:01 -0800

    start of main and testing

--- maskminder/__main__.py
+++ maskminder/__main__.py
@@ -2,6 +2,11 @@ from maskminder import note_instrument as ni
 from maskminder import scale_chord as sc
 
 
+def get_key_from_user():
+    key = raw_input('What key is your song?')
+    return key
+
+
 def main():
     note = ni.Note('G', 1)
     scale = sc.Scale('C', 'mixolydian')
