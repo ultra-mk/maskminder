410e70f5425bcc64fe992a1ffd7f81efc3b40420
michaelkunc <mjkunc@gmail.com>
2017-01-19 16:01:57 -0800

    start of main

--- maskminder/__main__.py
+++ maskminder/__main__.py
@@ -1,8 +1,12 @@
-import maskminder
+from maskminder import note_instrument as ni
+from maskminder import scale_chord as sc
 
 
 def main():
-    print(maskminder)
+    note = ni.Note('G', 1)
+    scale = sc.Scale('C', 'mixolydian')
+    print(note.frequency)
+    print(scale.notes)
 
 if __name__ == '__main__':
     main()
